6db5a123b658bb425de99b3a0defd29cb215146e
Jarek Figura <jarek.figura@icloud.com>
2018-04-12 12:45:54 +0200

    JS Koans

--- topics/about_prototypal_inheritance.js
+++ topics/about_prototypal_inheritance.js
@@ -6,27 +6,27 @@ module("About Prototypal Inheritance (topics/about_prototypal_inheritance.js)");
 // this 'class' pattern defines a class by its constructor
 var Mammal = function(name) {
     this.name = name;
-}
+};
 // things that don't need to be set in the constructor should be added to the constructor's prototype property.
 Mammal.prototype = {
     sayHi: function() {
         return "Hello, my name is " + this.name;
     }
-}
+};
 
 test("defining a 'class'", function() {
     var eric  = new Mammal("Eric");
-    equal(__, eric.sayHi(), 'what will Eric say?');
+    equal("Hello, my name is Eric", eric.sayHi(), 'what will Eric say?');
 });
 
 // add another function to the Mammal 'type' that uses the sayHi function
 Mammal.prototype.favouriteSaying = function() {
     return this.name + "'s favourite saying is " + this.sayHi(); 
-}
+};
 
 test("more functions", function() {
     var bobby = new Mammal("Bobby");
-    equal(__, bobby.favouriteSaying(), "what is Bobby's favourite saying?"); 
+    equal("Bobby's favourite saying is Hello, my name is Bobby", bobby.favouriteSaying(), "what is Bobby's favourite saying?");
 });
 
 test("calling functions added to a prototype after an object was created", function() {
@@ -36,7 +36,7 @@ test("calling functions added to a prototype after an object was created", funct
     };
     // the following statement asks the paul object to call a function that was added
     // to the Mammal prototype after paul was constructed.
-    equal(__, paul.numberOfLettersInName(), "how long is Paul's name?");
+    equal(4, paul.numberOfLettersInName(), "how long is Paul's name?");
 });
 
 // helper function for inheritance. 
